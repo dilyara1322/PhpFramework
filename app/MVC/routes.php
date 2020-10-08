32fa372ad89fcde14b087ae4f0448806e57b926d
Dilyara <dilyara_1322@mail.ru>
2020-10-08 16:18:53 +0300

    first views

--- app/MVC/routes.php
+++ app/MVC/routes.php
@@ -3,5 +3,6 @@
 /* require_once("../../framework/Routing/Route.php");
 require_once("../../framework/Routing/Router.php"); */
 
+Router::addRoute(new Route('', 'HelloController@index', Route::METHOD_GET));
 Router::addRoute(new Route('hello', 'HelloController@index', Route::METHOD_GET));
 Router::addRoute(new Route('data', 'HelloController@data', Route::METHOD_GET));
--- framework/Controller.php
+++ framework/Controller.php
@@ -2,5 +2,14 @@
 
 class Controller
 {
-    
+    protected function view($template_path, $data = [])
+    {
+        $template = file_get_contents('./app/MVC/Views/'.$template_path);
+        extract($data,EXTR_SKIP);
+        ob_start();
+        eval("?>".$template."<?");
+        $content = ob_get_contents();
+        ob_clean();
+        return $content;
+    }
 }
